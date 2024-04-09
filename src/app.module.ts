fa0abfcc150fef9000593078be07ce50a92d459a
j-brand <contact@blackbeetle.de>
2024-04-09 12:59:06 +0200

    add Database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ConfigModule } from '@nestjs/config';
+
 import { StoryModule } from './story/story.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-	imports: [StoryModule,
-		TypeOrmModule.forRoot(),
-	],
-	controllers: [AppController],
-	providers: [AppService],
+  imports: [
+    StoryModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PW,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-
-export class AppModule { }
+export class AppModule {}
